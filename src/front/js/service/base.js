ac68adf5a139f0f3992152a4e38f2598d95cb09c
lienzoenblanco <raquelgarciatrives@gmail.com>
2022-03-29 19:35:29 +0000

    Raquel :)

--- src/front/js/service/base.js
+++ src/front/js/service/base.js
@@ -1,5 +1,5 @@
 const hostName = window.location.hostname;
 const backHostName = hostName.replace("3000", "3001");
-console.log(backHostName)
+console.log(backHostName);
 
-export const BaseUrl = `https://${backHostName}/api`;
+export const BaseUrl = process.env.BACKEND_URL;
